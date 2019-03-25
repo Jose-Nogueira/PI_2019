36b48e91fca1ca8469b4593b7857bc0d6e4bf193
José Nogueira <jnogueira1994@hotmail.com>
2019-03-25 16:15:04 +0000

    1

--- Web_Server/Main_Menu.php
+++ Web_Server/Main_Menu.php
@@ -35,7 +35,7 @@
 
 <body>
   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"> Scient </a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="?pg=home"> Scient </a>
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap">
         <a class="nav-link" href="?logout=true">Sign out</a>
