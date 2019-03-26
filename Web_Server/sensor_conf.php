5a618b41f82ff08f340fc4bdbbf8d44c588347c5
Bruno <bpbarbosa42@gmail.com>
2019-03-26 18:09:05 +0000

    sensor_conf updated

--- Web_Server/sensor_conf.php
+++ Web_Server/sensor_conf.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="bootstrap-slider.js"></script>
 <?php
 $list = get_esps_info();
 for($i=0;$i< count($list);$i++){
@@ -9,7 +10,20 @@ for($i=0;$i< count($list);$i++){
 <h5 class="card-title" style="padding-top: 15px;">Esp <?php echo $esp_name . ", atual gold: " . $esp_gold;?></h5>
 
     <input name ="new_gold" type="range" class="custom-range" min="0" max="1024" id="customRange2", value="<?php echo $esp_gold;?>">
+    <input id="ex<?php echo $esp_id;?>" data-slider-id='ex1Slider' type="text" data-slider-min="0" data-slider-max="1024" data-slider-step="1" data-slider-value="14"/>
+    <span id="CurrentVAL">Current Value: <span id="SliderVal">3</span></span>
+
+
     <button type="button" class="btn btn-secondary" name="submit" value="<?php echo $esp_id;?>">Set</button>
+<script>
+
+// With JQuery
+$("#ex<?php echo $esp_id;?>").slider({
+	tooltip: 'always'
+});
+
+</script>
+
 </form>
 
 <?php
